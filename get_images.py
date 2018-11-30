d74acbd6b1598600b8176517f35c37cfb8e99738
LekaiSong <43052750+LekaiSong@users.noreply.github.com>
2018-11-30 06:03:21 -0500

    Update get_images.py

--- get_images.py
+++ get_images.py
@@ -2,28 +2,19 @@
 import tweepy
 import os
 import urllib
-import argparse
-##import wget
-##from bs4 import BeautifulSoup
 
 consumer_key = "TbOHFkGD6O6MkZmUwZaEJeFvS"
 consumer_secret = "g1wbA7SHlPXvKJASVnagWYHQYb3pC47smsIDkfeVX63OVHlMMS"
 access_key = "1038159139044614144-ux2em514t7bsDWRtjUm06LztrK090M"
 access_secret = "36ZjqZh8TeOGuzU7U1jpsmKZ5aOYew3QDKQYxuiTYechB"
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-u", "--user", required=True, help="user you're interested in")
-ap.add_argument("-i", "--images", required=True, help="amount of images you want to download")
-args = vars(ap.parse_args())
-
-def get_all_tweets(screen_name):
+def get_all_tweets(screen_name,image_num):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth)
         
     alltweets = []    
-    new_tweets = api.user_timeline(screen_name = screen_name,count=10)
-    #.append different?
+    new_tweets = api.user_timeline(screen_name,count=10)
     alltweets.extend(new_tweets)
     
     #save the id of the oldest tweet less one
@@ -33,14 +24,14 @@ def get_all_tweets(screen_name):
     while len(new_tweets) > 0:
         
         #all subsiquent requests use the max_id param to prevent duplicates
-        new_tweets = api.user_timeline(screen_name = screen_name,count=10,max_id=oldest)
+        new_tweets = api.user_timeline(screen_name,count=50,max_id=oldest)
     
         #save most recent tweets
         alltweets.extend(new_tweets)
         
         #update the id of the oldest tweet less one
         oldest = alltweets[-1].id - 1
-        if(len(alltweets) > int(args["images"])):
+        if(len(alltweets) > int(image_num)):
             break
         print("...%s tweets downloaded so far" % (len(alltweets)))
 
@@ -50,11 +41,9 @@ def get_all_tweets(screen_name):
         if (len(media)>0):
             tweetsmedia.add(media[0]['media_url'])
     
-    #download images named 01~25
+    #download images
     i=0
     for url in tweetsmedia:
         print(url)
-        urllib.request.urlretrieve(url, os.getcwd()+'/%02d.jpg'%i)
+        urllib.request.urlretrieve(url, os.getcwd()+'/%03d.jpg'%i)
         i += 1
-    ##for url in tweetsmedia:
-        ##wget.download -c 'url' -O 'image%d'%i
